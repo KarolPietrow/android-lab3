f8d78c1649265632791a025aacc4c0b6a036a82c
Karol Pietrów <karolpietrow2004@gmail.com>
2024-11-04 17:05:38 +0100

    v.1.0 All required functionality complete

--- app/src/main/java/pl/karolpietrow/kp3/MainActivity.kt
+++ app/src/main/java/pl/karolpietrow/kp3/MainActivity.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
